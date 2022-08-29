0d26b1fef81a8e705406435481f9caa416cb0b19
TheWiseKo <97468713+TheWiseKo@users.noreply.github.com>
2022-08-30 04:22:09 +0900

    Add files via upload

--- user/forms.py
+++ user/forms.py
@@ -36,27 +36,6 @@ class RegisterForm(UserCreationForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(
-        error_messages={"required": "유저이름을 입력해주세요."},
-        label="유저명",
-    )
-    password = forms.CharField(
-        error_messages={"required": "비밀번호를 입력해주세요."},
-        widget=forms.PasswordInput,
-        label="비밀번호",
-    )
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get("username")
-        password = cleaned_data.get("password")
-
-        if username and password:
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                self.add_error("username", "아이디가 없습니다")
-                return
-
-            if not check_password(password, user.password):
-                self.add_error("password", "비밀번호를 틀렸습니다")
+    # TODO: 2. login 할 때 form을 활용해주세요
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import LoginForm, RegisterForm
 
@@ -26,31 +26,43 @@ def register_view(request):
 
 
 def login_view(request):
+    error = None
     if request.method == "POST":
+        # TODO: 1. /login로 접근하면 로그인 페이지를 통해 로그인이 되게 해주세요
+        # TODO: 2. login 할 때 form을 활용해주세요						
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get("username")
-            user = User.objects.get(username=username)
-            login(request, user)
-            request.session["user"] = username
-            return HttpResponseRedirect("/")
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                error = "올바른 이메일 주소와 비밀번호를 입력하세요."
+                pass
+            else:
+                if user.check_password(password):
+                    error = None
+                    login(request, user)
+                    return redirect("index")
     else:
         form = LoginForm()
-    return render(request, "login.html", {"form": form})
+    return render(request, "login.html", {"form": form, "error": error})
 
 
 def logout_view(request):
-    if "user" in request.session:
-        del request.session["user"]
-    logout(request)
-    return HttpResponseRedirect("/login")
+    # TODO: 3. /logout url을 입력하면 로그아웃 후 / 경로로 이동시켜주세요
+    logout(request)						
+    return redirect("index")
 
 
+# TODO: 8. user 목록은 로그인 유저만 접근 가능하게 해주세요
 @login_required(login_url="/login")
 def user_list_view(request):
-    page = int(request.GET.get("page", 1))
-    users = User.objects.all().order_by("-id")
-    paginator = Paginator(users, 3)
+    # TODO: 7. /users 에 user 목록을 출력해주세요
+    # TODO: 9. user 목록은 pagination이 되게 해주세요
+    users = User.objects.all()
+    paginator = Paginator(users, 10)
+    page = request.GET.get('page')
     users = paginator.get_page(page)
 
     return render(request, "users.html", {"users": users})
